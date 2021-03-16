04b29cbf648912fae15e76c66fbf6ab4a5951e28
Brianti Bella Prasnitia <74761967+briantibellaprasnitia@users.noreply.github.com>
2021-03-16 20:12:16 +0700

    Add files via upload

---
+++ E41202408_Brianti Bella Prasnitia_Golongan D/ManagingPeople/src/managingpeople/Person.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managingpeople;
+
+/**
+ *
+ * @author Brianti Bella Prasnitia
+ */
+class Person {
+    String name;
+    int age;
+    
+    public Person(String name, int age)
+{
+      this.name = name;
+      this.age = age;
+    }
+    public String getName(){
+      return name;
+    }
+    public int getAge(){
+      return age;
+    }
+    public void setAge(int age){
+      this.age = age;
+    }
+    
+}
---
+++ E41202408_Brianti Bella Prasnitia_Golongan D/Person/src/person/Person.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person;
+
+/**
+ *
+ * @author Brianti Bella Prasnitia
+ */
+public class Person {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String fName, IName, stuStatus;
+        int stuId;
+        
+        fName = "Lisa Palombo";
+        IName = "Lisa";
+        stuStatus = "Active";
+        stuId = 123456789;
+        
+        System.out.println("Student Name : " + fName);
+        System.out.println("IName : " + IName);
+        System.out.println("Student Status : " + stuStatus);
+        System.out.println("Student ID : " + stuId);
+    }
+    
+}
