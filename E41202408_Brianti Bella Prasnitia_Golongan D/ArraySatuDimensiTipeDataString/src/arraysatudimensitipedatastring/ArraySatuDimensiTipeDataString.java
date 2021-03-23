4641965691421862fd003797ffec91409020b328
Brianti Bella Prasnitia <74761967+briantibellaprasnitia@users.noreply.github.com>
2021-03-23 15:28:15 +0700

    Add files via upload

---
+++ E41202408_Brianti Bella Prasnitia_Golongan D/ArraySatuDimensiTipeDataString/src/arraysatudimensitipedatastring/ArraySatuDimensiTipeDataString.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arraysatudimensitipedatastring;
+
+/**
+ *
+ * @author Brianti Bella Prasnitia
+ */
+public class ArraySatuDimensiTipeDataString {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[] teks = {"Brianti Bella"," ","Prasnitia"};
+        
+        for(int j=0; j<teks.length; j++){
+            System.out.print(teks[j]);
+            System.out.print("");
+        }
+    }
+    
+}
